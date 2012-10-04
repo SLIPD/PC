0d620867a72c4057e8f80da4bd7ec025c31ab0a5
Velox <DaleMyers19@gmail.com>
2012-10-04 11:58:01 +0100

    Added base files with comments explaining what each one does

---
+++ pi/compression.py
@@ -0,0 +1,1 @@
+'''This program will listen for data from a socket, remove redundant data, compress it and send it on to the network program'''
---
+++ pi/interface.py
@@ -0,0 +1,1 @@
+'''This file is the interface between the speck and the pi. It will read the data and send it on to another port'''
