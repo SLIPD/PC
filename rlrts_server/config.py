198195e99710958e6389b5b3fe441cbb1c5f1121
Kit Barnes <kit@ninjalith.com>
2012-10-07 21:13:31 +0100

    Flesh out server implementation

--- rlrts_server/config.py
+++ rlrts_server/config.py
@@ -1,1 +1,2 @@
 MESH_SQUARE_SIDE = 2.5  # Metres
+N_TERRITORIES = 9  # Make it square
--- rlrts_server/world.py
+++ rlrts_server/world.py
@@ -1,4 +1,5 @@
 from rlrts_server import config
+from math import floor
 import numpy
 
 
@@ -6,15 +7,28 @@ class Unit(object):
     def __init__(self, team, (x, y)):
         self.team = team
         self.coords = (x, y)
+        self.world = None
 
     def move(self, (x, y)):
         self.coords = (x, y)
 
+    def set_world(self, world):
+        self.world = world
+
+    def get_mesh_indices(self):
+        return map(lambda n: round(n / config.MESH_SQUARE_SIDE), self.coords)
+
+    def get_step_indices(self):
+        return map(lambda n: floor(n / config.MESH_SQUARE_SIDE), self.coords)
+
 
 class Team(object):
-    def __init__(self, connection, units):
-        self.connection = connection
+    def __init__(self, units):
         self.units = units
+
+    def set_world(self, world):
+        self.world = world
+        [u.set_world(world) for u in self.units]
 
 
 class World(object):
@@ -22,6 +36,8 @@ class World(object):
         self.teams = teams
         self.dimensions = (self.width, self.height) = (width, height)
         self.base_location = (self.base_x, self.base_y) = (base_x, base_y)
+
+        [t.set_world(self) for t in teams]
 
         self._init_mesh()
 
